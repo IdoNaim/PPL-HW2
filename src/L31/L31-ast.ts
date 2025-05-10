649284167d57c361d07761ab66a8653a471f90eb
IdoNaim <idonai@post.bgu.ac.il>
2025-05-10 18:02:12 +0300

    eyal helped 2.1 workess yayyyy

--- src/L31/L31-ast.ts
+++ src/L31/L31-ast.ts
@@ -67,6 +67,8 @@ export type Binding = {tag: "Binding"; var: VarDecl; val: CExp; }
 export type LetExp = {tag: "LetExp"; bindings: Binding[]; body: CExp[]; }
 // L3
 export type LitExp = {tag: "LitExp"; val: SExpValue; }
+export type DictExp = {tag: "DictExp"; val: SExpValue[]; } 
+
 
 // Type value constructors for disjoint types
 export const makeProgram = (exps: Exp[]): Program => ({tag: "Program", exps: exps});
@@ -111,6 +113,8 @@ export const isBinding = (x: any): x is Binding => x.tag === "Binding";
 export const isLetExp = (x: any): x is LetExp => x.tag === "LetExp";
 // L3
 export const isLitExp = (x: any): x is LitExp => x.tag === "LitExp";
+export const isDictExp = (x: any): x is LitExp => x.tag === "LitExp" && x.val.tag === "DictExp";
+
 
 // Type predicates for type unions
 export const isExp = (x: any): x is Exp => isDefineExp(x) || isCExp(x);
@@ -118,7 +122,7 @@ export const isAtomicExp = (x: any): x is AtomicExp =>
     isNumExp(x) || isBoolExp(x) || isStrExp(x) ||
     isPrimOp(x) || isVarRef(x);
 export const isCompoundExp = (x: any): x is CompoundExp =>
-    isAppExp(x) || isIfExp(x) || isProcExp(x) || isLitExp(x) || isLetExp(x);
+    isAppExp(x) || isIfExp(x) || isProcExp(x) || isLitExp(x) || isLetExp(x) || isDictExp(x);
 export const isCExp = (x: any): x is CExp =>
     isAtomicExp(x) || isCompoundExp(x);
 
@@ -306,7 +310,7 @@ export const parseSExp = (sexp: Sexp): Result<SExpValue> =>
     isString(sexp) ? makeOk(makeSymbolSExp(sexp)) :
     sexp.length === 0 ? makeOk(makeEmptySExp()) :
     isDottedPair(sexp) ? makeDottedPair(sexp) :
-    isDict(sexp) ? makeDict(sexp) :
+    // isDict(sexp) ? makeDict(sexp) :
     isNonEmptyList<Sexp>(sexp) ? (
         // fail on (x . y z)
         first(sexp) === '.' ? makeFailure(`Bad dotted sexp: ${format(sexp)}`) : 
