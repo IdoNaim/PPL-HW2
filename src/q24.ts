8ba6efdd0824cb083b37067314d9f8e9600f8449
IdoNaim <idonai@post.bgu.ac.il>
2025-05-13 23:02:20 +0300

    2.5 a completed i think

--- src/q24.ts
+++ src/q24.ts
@@ -1,12 +1,55 @@
-import { makeProgram, Program } from './L3/L3-ast';
+import { makeProgram, Program ,Exp, CExp, makeAppExp, makeVarRef, CompoundExp, makeLitExp, parseSExp} from './L32/L32-ast';
+import {makeCompoundSExp, CompoundSExp} from './L32/L32-value';
+import {DictExp, isDictExp, Binding ,unparseL32} from './L32/L32-ast';
+import {is, map} from "ramda";
+import { first, second, rest, allT, isEmpty, isNonEmptyList, List, NonEmptyList } from "./shared/list";
+import { makeEmptySExp, SExpValue, makeSymbolSExp } from './L32/L32-value';
+import {evalSequence}  from './L32/L32-eval';
+import { makeEmptyEnv } from './L32/L32-env';
+import { parse as p, isSexpString, isToken, isCompoundSexp } from "./shared/parser";
+import { Result, makeOk, makeFailure, bind, mapResult, mapv, isOk } from "./shared/result";
+
+
+
 /*
 Purpose: rewrite all occurrences of DictExp in a program to AppExp.
 Signature: Dict2App (exp)
 Type: Program -> Program
 */
 export const Dict2App  = (exp: Program) : Program =>
-    //@TODO
-    makeProgram([]);
+    makeProgram(map(turn2App,exp.exps));
+
+export const turn2App = (exp: Exp) : Exp|CompoundExp => 
+    isDictExp(exp) ? makeAppExp(makeVarRef("dict"), makeKVlist(exp)) : exp
+
+const makeKVlist = (dictExp :DictExp)  :CExp[]=> {
+    const list = makeList(dictExp.entries)
+    return [makeLitExp(list)]
+}
+
+const makeList = (entries: Binding[]): SExpValue =>
+    isNonEmptyList<Binding>(entries) ? makeCompoundSExp(makeCompoundSExp(makeSymbolSExp(entries[0].var.var), getStringVal(unparseL32(entries[0].val))), makeList(entries.slice(1))) 
+: makeEmptySExp();
+
+const getStringVal=(val : string) : SExpValue => {
+    const sexp = p(val);
+    if(isOk(sexp)){
+        const parsed = parseSExp(sexp.value);
+        if(isOk(parsed)){
+            return parsed.value;
+        }
+        return "bad"
+    }
+    return "bad"
+}
+    
+
+// export const listPrim = (vals: List<Value>): EmptySExp | CompoundSExp =>
+//     isNonEmptyList<Value>(vals) ? makeCompoundSExp(first(vals), listPrim(rest(vals))) :
+//     makeEmptySExp();
+    
+
+
 
 
 /*
--- test.ts
+++ test.ts
@@ -1,21 +1,36 @@
 import { parse as p, isSexpString, isToken, isCompoundSexp } from "./src/shared/parser";
 import {  evalL32program } from "./src/L32/L32-eval";
-import { Value } from "./src/L32/L32-value";
+import { Value ,CompoundSExp} from "./src/L32/L32-value";
 import { Result, bind, isFailure, isOk, makeFailure, makeOk } from "./src/shared/result";
-import { parseL32, parseL32Exp } from "./src/L32/L32-ast";
+import { Exp} from "./src/L32/L32-ast";
+import { parseL32, parseL32Exp, Program, isProgram ,unparseL32, isLitExp} from "./src/L32/L32-ast";
+import {turn2App} from "./src/q24";
+// console.log(p("(d 'b)"))
 
-console.log(p("(d 'b)"))
-
-const evalP = (x: string): Result<Value> =>
-    bind(parseL32(x), evalL32program);
-console.log(p(`(L32 ((dict (a 1) (b 2)) 'a))`))
-const sexpOfDict = p(`((dict (a 1) (b 2)) 'a)`)
-console.log(sexpOfDict)
-const val = isOk(sexpOfDict) ? sexpOfDict.value : "bad"
-console.log(parseL32Exp(val))
-console.log(evalP(`(L32 ((dict (a 1) (b 2)) 'a))`))
-console.log()
+// const evalP = (x: string): Result<Value> =>
+//     bind(parseL32(x), evalL32program);
+// console.log(p(`(L32 ((dict (a 1) (b 2)) 'a))`))
+// const sexpOfDict = p(`((dict (a 1) (b 2)) 'a)`)
+// console.log(sexpOfDict)
+// const val = isOk(sexpOfDict) ? sexpOfDict.value : "bad"
+// console.log(parseL32Exp(val))
+// console.log(evalP(`(L32 ((dict (a 1) (b 2)) 'a))`))
+// console.log()
+// const val = isOk(evalP(`(L32 ((dict (a 1) (b 2)) 'a))`)) ? evalP(`(L32 ((dict (a 1) (b 2)) 'a))`).value : "bad"
 // "(L32 ((dict (a 1) (b 2)) 'a))"
+
+
+const prog = parseL32(`(L32 (dict (a 1) (b 2)))`)
+const dict = bind(prog, (prog: Program) => isProgram(prog) ? makeOk((prog as Program).exps[0]): makeFailure("not a program") )
+const dictExp = isOk(dict) ? dict.value : "bad"
+const appExp = turn2App(dictExp as Exp)
+// console.log(unparseL32((dictExp as Exp)))
+console.log(unparseL32(appExp))
+
+// const prog2 = parseL32(`(L32 '((a . 1) (b . 2)))`)
+// const lit = isOk(prog2) ? isProgram(prog2.value) ? prog2.value.exps[0] : "bad" : "bad"
+// isLitExp(lit) ? console.log(unparseL32(lit)) : console.log("not a litExp")
+// isLitExp(lit) ? console.log((lit.val as CompoundSExp).val1) : console.log("not a litexp")
 
 
 
